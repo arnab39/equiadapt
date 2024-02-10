479543404916d6ee2ca144685e890dcffce57827
arnab39 <arnab.mondal@cn-d001.server.mila.quebec>
2024-02-10 02:36:02 -0500

    new restructure working

---
+++ equiadapt/images/canonicalization/continuous_group.py
@@ -0,0 +1,11 @@
+import math
+import torch
+import kornia as K
+from equiadapt.common.basecanonicalization import BaseCanonicalization
+from equiadapt.images.utils import roll_by_gather
+from torchvision import transforms
+
+
+
+
+        
