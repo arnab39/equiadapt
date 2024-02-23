47f2a387d5c71840496b0ee5b8918b2241446336
danibene <34680344+danibene@users.noreply.github.com>
2024-02-23 00:22:09 -0500

    update location of module directory

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
-module_dir = os.path.join(__location__, "../src/equiadapt")
+module_dir = os.path.join(__location__, "../equiadapt")
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
