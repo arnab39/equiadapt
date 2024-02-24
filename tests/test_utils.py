ca8205d27d29763f3240a2dd9b306b6ce7388906
Danielle Benesch <daniellerbenesch@gmail.com>
2024-02-23 20:20:59 -0500

    add regression test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,16 +4,10 @@ from equiadapt.common.utils import gram_schmidt
 
 
 def test_gram_schmidt() -> None:
+    torch.manual_seed(0)
     vectors = torch.randn(1, 3, 3)  # batch of 1, 3 vectors of dimension 3
-    orthogonal_vectors = gram_schmidt(vectors)
 
-    # Check if the vectors are orthogonal
-    for i in range(orthogonal_vectors.shape[1]):
-        for j in range(i + 1, orthogonal_vectors.shape[1]):
-            dot_product = torch.dot(orthogonal_vectors[0, i], orthogonal_vectors[0, j])
-            assert abs(dot_product.item()) < 1e-6
+    output = gram_schmidt(vectors)
 
-    # Check if the vectors are normalized
-    for i in range(orthogonal_vectors.shape[1]):
-        norm = torch.norm(orthogonal_vectors[0, i]).item()
-        assert abs(norm - 1) < 1e-6
+    assert torch.allclose(output[0][0][0], torch.tensor(0.5740), atol=1e-4)
+
