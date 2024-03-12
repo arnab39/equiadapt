7c9e2c165786ef9166e8b1c9e4545c4ce0d72387
arnab39 <arnab.mondal@mail.mcgill.ca>
2024-03-12 15:56:14 -0400

    run mkinit

---
+++ equiadapt/pointcloud/canonicalization/__init__.py
@@ -0,0 +1,8 @@
+from equiadapt.pointcloud.canonicalization import continuous_group
+
+from equiadapt.pointcloud.canonicalization.continuous_group import (
+    ContinuousGroupPointcloudCanonicalization,
+    EquivariantPointcloudCanonicalization,)
+
+__all__ = ['ContinuousGroupPointcloudCanonicalization',
+           'EquivariantPointcloudCanonicalization', 'continuous_group']
