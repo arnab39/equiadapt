a228268f807b9e328af702305f75300302b8f6da
D. Benesch <34680344+danibene@users.noreply.github.com>
2024-03-13 23:39:01 -0400

    add more type hints to test_continuous_group.py

--- tests/images/canonicalization/test_continuous_group.py
+++ tests/images/canonicalization/test_continuous_group.py
@@ -1,28 +1,25 @@
+from typing import Any, Dict
 from unittest.mock import Mock
 
 import pytest
 import torch
 from omegaconf import DictConfig
+from torch import Tensor
 
-from equiadapt import (  # Update with your actual import path
-    ContinuousGroupImageCanonicalization,
-)
-
+from equiadapt import ContinuousGroupImageCanonicalization
 
 @pytest.fixture
-def sample_input():
+def sample_input() -> Tensor:
     # Create a sample input tensor
     return torch.rand((1, 3, 64, 64))  # A batch with one color image of size 64x64
 
-
 @pytest.fixture
-def grayscale_input():
+def grayscale_input() -> Tensor:
     # Create a grayscale input tensor
     return torch.rand((1, 1, 64, 64))  # A batch with one grayscale image of size 64x64
 
-
 @pytest.fixture
-def init_args() -> dict:
+def init_args() -> Dict[str, Any]:
     # Mock initialization arguments
     canonicalization_hyperparams = DictConfig(
         {
@@ -36,15 +33,13 @@ def init_args() -> dict:
         "in_shape": (3, 64, 64),
     }
 
-
-def test_initialization(init_args) -> None:
+def test_initialization(init_args: Dict[str, Any]) -> None:
     cgic = ContinuousGroupImageCanonicalization(**init_args)
     assert cgic.pad is not None, "Pad should be initialized."
     assert cgic.crop is not None, "Crop should be initialized."
 
-
 def test_transformation_before_canonicalization_network_forward(
-    sample_input, init_args
+    sample_input: Tensor, init_args: Dict[str, Any]
 ) -> None:
     cgic = ContinuousGroupImageCanonicalization(**init_args)
     transformed = cgic.transformations_before_canonicalization_network_forward(
@@ -54,15 +49,14 @@ def test_transformation_before_canonicalization_network_forward(
         [1, 3, 32, 32]
     ), "The transformed image should be resized to (32, 32)."
 
-
 @pytest.fixture
-def canonicalization_instance():
+def canonicalization_instance() -> ContinuousGroupImageCanonicalization:
     instance = ContinuousGroupImageCanonicalization(
         canonicalization_network=Mock(),
-        canonicalization_hyperparams={
+        canonicalization_hyperparams=DictConfig({
             "input_crop_ratio": 0.9,
             "resize_shape": (32, 32),
-        },
+        }),
         in_shape=(3, 64, 64),
     )
     # Mocking the get_groupelement method to return a fixed group element
