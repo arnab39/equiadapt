beea684159dd9439836b0213cd1e420cbc7ac884
danibene <34680344+danibene@users.noreply.github.com>
2024-03-14 19:50:46 -0400

    Revert "add postprocessing to conf.py"
    This reverts commit 3cee75d8fd81e66ab430cdd7e09c33b46f11213d.

--- docs/conf.py
+++ docs/conf.py
@@ -12,18 +12,6 @@ import shutil
 import sys
 from pathlib import Path
 import shutil
-import argparse  # Import the argparse module
-import re
-from pathlib import Path
-from typing import Match, Union
-
-from bs4 import BeautifulSoup, Tag
-
-try:
-    from emoji import emojize
-except ImportError:
-    print("Error: package not found, install 'emoji' package with 'pip install emoji'")
-
 
 # -- Path setup --------------------------------------------------------------
 
@@ -330,50 +318,3 @@ intersphinx_mapping = {
 }
 
 print(f"loading configurations for {project} {version} ...", file=sys.stderr)
-
-def match_to_emoji(m: Match[str]) -> str:
-    """Call emoji.emojize on m)."""
-    return emojize(m.group())
-
-
-def emojize_all(s: str) -> str:
-    """Convert all emojis :aliases: of the string s to emojis in UTF-8."""
-    return re.sub(r":([a-z_-]+):", match_to_emoji, s)
-
-
-def update_image_paths(soup: BeautifulSoup) -> None:
-    """Update src attribute of img tags starting with 'utils/'."""
-    imgs = soup.find_all("img", src=re.compile("^utils/"))
-    for img in imgs:
-        if isinstance(img, Tag):  # Ensuring that 'img' is a Tag object
-            img["src"] = img["src"].replace("utils/", "_images/")
-
-
-def process_html_file(html_file: Union[str, Path]) -> None:
-    """Process an individual HTML file."""
-    with open(html_file, "r", encoding="utf-8") as file:
-        content = file.read()
-
-    content = emojize_all(content)
-    soup = BeautifulSoup(content, "html.parser")
-    update_image_paths(soup)
-
-    with open(html_file, "w", encoding="utf-8") as file:
-        file.write(str(soup))
-
-def postprocess(app, exception):
-    __location__: Path = Path(__file__).parent
-    base_path = __location__ / "_build" / "html"
-
-    html_files: list[Path] = list(base_path.glob("*.html"))
-
-    for html_file in html_files:
-        print(f"Processing {html_file}...")
-        process_html_file(html_file)
-
-    print("HTML postprocessing completed.")
-    # stuff to edit and save the html
-
-def setup(app):
-    app.connect('build-finished', postprocess)
-
