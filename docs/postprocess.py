d38d24e389029f86c255db4848614dcc75979f9d
danibene <34680344+danibene@users.noreply.github.com>
2024-03-14 11:07:49 -0400

    replace emojis in a similarly hacky way

--- docs/postprocess.py
+++ docs/postprocess.py
@@ -1,6 +1,25 @@
 import os
+import re
 
 from bs4 import BeautifulSoup
+
+# Install from https://github.com/carpedm20/emoji/
+# with pip install emoji
+try:
+    from emoji import emojize
+except ImportError:
+    print("Error: package not found, install 'emoji' package with 'pip install emoji'")
+
+
+def match_to_emoji(m):
+    """Call emoji.emojize on m)."""
+    return emojize(m.group())
+
+
+def emojize_all(s):
+    """Convert all emojis :aliases: of the string s to emojis in UTF-8."""
+    return re.sub(r":([a-z_-]+):", match_to_emoji, s)
+
 
 # Specify the pattern to match the HTML files you want to postprocess
 __location__ = os.path.dirname(__file__)
@@ -9,7 +28,12 @@ html_files = [os.path.join(__location__, "_build", "html", "readme.html")]
 
 for html_file in html_files:
     with open(html_file, "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file, "html.parser")
+        content = file.read()
+
+    # Convert emojis in the entire HTML content
+    content = emojize_all(content)
+
+    soup = BeautifulSoup(content, "html.parser")
 
     # Find all <img> tags with the specific src attribute
     imgs = soup.find_all("img", {"src": "utils/equiadapt_logo.png"})
